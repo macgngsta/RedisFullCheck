8fb342e880d8dc04f8a54d1ca6358ec356c9cd80
Greg Tam <greg.tam@moderntreasury.com>
2024-04-18 21:01:34 -0700

    [FDN-2908] add skipping

--- ruby/ttl_repair.rb
+++ ruby/ttl_repair.rb
@@ -23,6 +23,7 @@ target_memdb = Redis::Cluster.new(nodes: [target_memorydb_endpoint], reconnect_a
 
 cursor = 0
 updated_keys = []
+skipped_keys = []
 scanned_keys = 0
 
 p "#{Time.now} START"
@@ -33,19 +34,37 @@ p(Benchmark.measure do
     cursor, keys = target_memdb.scan(cursor, count: BATCH_SIZE, type: 'string')
 
     # bulk get TTL of keys from memdb
-    memdb_exp_values = target_memdb.pipelined do |pipelined|
-      keys.each { |k| pipelined.pexpiretime(k) }
+    memdb_exp_values = target_memdb.pipelined do |mem_pipelined|
+      keys.each do |k|
+        if mem_pipelined.respond_to?(:pexpiretime)
+          mem_pipelined.pexpiretime(k)
+        else
+          # add to skipped
+          skipped_keys << "memdb:#{k}"
+          -1
+        end
+      end
     end
 
     # bulk get TTL / expiretime of keys from elasticache
-    e_exp_values = source_elasticache.pipelined do |pipelined|
-      keys.each { |k| pipelined.pexpiretime(k) }
+    e_exp_values = source_elasticache.pipelined do |ec_pipelined|
+      keys.each do |k|
+        # we assume that if mem db doesnt have expires, ec wont have it either
+        if ec_pipelined.respond_to?(:pexpiretime)
+          ec_pipelined.pexpiretime(k)
+        else
+          skipped_keys << "ec:#{k}"
+          -1
+        end
+      end
     end
 
     # bulk update expiretime for any differences
     target_memdb.pipelined do |pipelined|
       memdb_exp_values.each_with_index do |memdb_val, i|
         e_val = e_exp_values[i]
+
+        next if e_val == -1
 
         p "e_exp: #{e_val} memdb_exp: #{memdb_val}"
         next unless (memdb_val - e_val).abs > LATENCY_BUFFER
@@ -68,4 +87,5 @@ p ''
 p "#{Time.now} FINISHED"
 p "number of keys scanned: #{scanned_keys}"
 p "number of key TTLs updated if not dryrun: #{updated_keys.size}"
+p "skipped keys: #{skipped_keys}"
 p updated_keys
